df9a933e5314ac6169e870302dfe8fd4e8f6dc7f
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-05-27 13:12:37 +0100

    can get multiple pages from api

--- client/src/models/cities.js
+++ client/src/models/cities.js
@@ -4,15 +4,25 @@ const PubSub = require(`../helpers/pubsub.js`);
 
 const Cities = function (url) {
   this.url = url;
-  this.request = new RequestHelper(this.url)
-  this.cities_request = new CitiesRequestHelper(this.url)
+  // this.request = new RequestHelper(this.url)
+
 }
 
-Cities.prototype.getData = function () {
+Cities.prototype.getData = function (counter = 0) {
+  if (counter < 2) {
+  this.cities_request = new CitiesRequestHelper(this.url)
   this.cities_request.get()
-    .then((cities) => {
-      console.log(cities);
-    })
+  .then((cities) => {
+    // citiesData.push(cities);
+    // TODO: In some way put cities.data into the database here
+    this.url = `https://wft-geo-db.p.rapidapi.com${cities.links[1].href}`;
+    console.log(counter, cities);
+    counter++;
+    this.getData(counter);
+  })}
+    // console.log(citiesData);
+    // this.url =
+  // console.log(citiesData);
 };
 
 module.exports = Cities;
