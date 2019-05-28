2da4649d463c9b3baf66cb627454521ac1825b91
James <james___3248@hotmail.com>
2019-05-28 14:16:01 +0100

    displaying content in my cities

--- client/src/views/my_cities_view.js
+++ client/src/views/my_cities_view.js
@@ -12,6 +12,12 @@ MyCitiesView.prototype.render = function (city) {
     const name = this.createHeading(city.name)
     cityContainer.appendChild(name)
 
+    const rating = this.createRating(city.rating)
+    cityContainer.appendChild(rating)
+
+    const review = this.createReview(city.review)
+    cityContainer.appendChild(review)
+
     this.container.appendChild(cityContainer)
 
 }
@@ -21,5 +27,17 @@ MyCitiesView.prototype.createHeading = function(textContent) {
     heading.textContent = textContent
     return heading
 }
+
+MyCitiesView.prototype.createRating = function (textContent) {
+  const rating = document.createElement('h4')
+  rating.textContent = textContent
+  return rating
+};
+
+MyCitiesView.prototype.createReview = function (textContent) {
+  const review = document.createElement('p')
+  review.textContent = textContent
+  return review
+};
 
 module.exports = MyCitiesView;
